2383499e525ed22a40c6915685b8439b80d248fb
Guilherme Bastos <guibastos3003@gmail.com>
2025-10-28 17:44:55 -0300

    Correção do main.js

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-var http = require('http');
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT;
-app.listen(port)
 
-const baseDir = `${process.env.DIR_NAME}/dist/`
-app.use(express.static(`${baseDir}`))
-app.get('/', (req, res) => res.sendfile('index.html', { root: baseDir }))
+const port = process.env.PORT || 3000;
+
+const baseDir = path.join(__dirname, 'dist');
+
+app.use(express.static(baseDir));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(baseDir, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor iniciado e ouvindo na porta ${port}`);
+});
