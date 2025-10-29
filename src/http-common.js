457512bdc37d36e60563441235d3fda4418b5cda
Guilherme Bastos <guibastos3003@gmail.com>
2025-10-29 17:21:27 -0300

    Ajuste da URL da API

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import AuthService from "./services/authDataService";
 
 const apiClient = axios.create({
 
-    baseURL: process.env.VITE_REACT_APP_API_URL || "http://localhost:3000",
+    baseURL: process.env.VITE_API_URL || "http://localhost:3000",
     headers: {
         "Content-type": "application/json"
     }
